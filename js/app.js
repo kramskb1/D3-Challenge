c51a4ab1ea650048811bb597e470bbe713cc4e9b
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2021-01-11 10:30:32 -0500

    last commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var svgWidth = 800;
+var svgHeight = 500;
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+  };
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+var svg = d3
+.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
